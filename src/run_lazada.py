87c6765ee08e77e5f1fcf176bb78c0b343ded828
kahayashi <hongphat098@gmail.com>
2021-04-07 23:03:21 -0600

    test

--- src/run_lazada.py
+++ src/run_lazada.py
@@ -5,5 +5,43 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException
 
 
-driver = webdriver.Chrome(executable_path='chromedriver')
-driver.get('https://www.lazada.sg/#')
+import os
+os.environ["LANG"] = "en_US.UTF-8"
+
+
+driver = webdriver.Chrome(executable_path='E:\Download\chromedriver_win32\chromedriver.exe')
+driver.get('https://www.spectrum.com/mobile')
+
+
+timeout = 30
+try:
+    WebDriverWait(driver, timeout).until(EC.visibility_of_element_located((By.ID, "Level_1_Category_No1")))
+except TimeoutException:
+    driver.quit()
+
+
+category_element = driver.find_element(By.ID,'Level_1_Category_No1').text
+#result -- Electronic Devices as the first category listing
+
+list_category_elements = driver.find_element(By.XPATH,'//*[@id="J_icms-5000498-1511516689962"]/div/ul')
+links = list_category_elements.find_elements(By.CLASS_NAME,"lzd-site-menu-root-item")
+for i in range(len(links)):
+    print("element in list ",links[i].text)
+#result {Electronic Devices, Electronic Accessories, etc}
+
+
+element = driver.find_elements_by_class_name('J_ChannelsLink')[1]
+webdriver.ActionChains(driver).move_to_element(element).click(element).perform()
+
+
+head = driver.find_elements_by_tag_name('head')
+for word in head:
+    if word.text != "":
+        print(word.text)
+
+
+words = driver.find_elements_by_tag_name('h2')
+for count, w in enumerate(words):
+    if count <= 1000:
+        if w.text != "":   
+            print(w.text)
--- src/script.py
+++ src/script.py
@@ -1,29 +1,135 @@
 import requests
 from bs4 import BeautifulSoup
-#import operator
-#from collections import Counter
-import pandas as pd
+import operator
+from collections import Counter
 import numpy as np
+import pandas as pd
 
 
-headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.71 Safari/537.36'}
-url = "https://en.wikipedia.org/wiki/List_of_national_capitals"
-r = requests.get(url, headers=headers)
+'''
+import requests
+import logging
+
+import http.client
+http.client.HTTPConnection.debuglevel = 1
+
+# You must initialize logging, otherwise you'll not see debug output.
+logging.basicConfig()
+logging.getLogger().setLevel(logging.DEBUG)
+requests_log = logging.getLogger("requests.packages.urllib3")
+requests_log.setLevel(logging.DEBUG)
+requests_log.propagate = True
+'''
+
+page = requests.get("https://wikipedia.com/").text
+soup = BeautifulSoup(page,'lxml')
+print(soup.prettify())
+
+print(soup.get_text())
+
+wordlist = []
+
+words = soup.get_text().split()
+
+for word in words:
+    if word != "":
+        wordlist.append(word)
+
+dict_ = {'key': 0 }
+dict_.append
+
+for key in dict_.key():
+    for word in wordlist:
+        if key != word:
+            dict_.append(key, 1)
+        else:
+            dict_.append(key, (dict_.values()+1))
+    
+
+        
 
 
-soup = BeautifulSoup(r.content, "html.parser")
-table = soup.find_all('table')[1]
-rows = table.find_all('tr')
-row_list = list()
-
-for tr in rows:
-    td = tr.find_all('td')
-    row = [i.text for i in td]
-    row_list.append(row)
 
 
-df_bs = pd.DataFrame(row_list,columns=['City','Country','Notes'])
-df_bs.set_index('Country',inplace=True)
 
-df_bs.head
-#df_bs.to_csv('beautifulsoup.csv')
+
+
+
+def start(url):
+ 
+    # empty list to store the contents of
+    # the website fetched from our web-crawler
+    wordlist = []
+    source_code = requests.get(url).text
+ 
+    # BeautifulSoup object which will
+    # ping the requested url for data
+    soup = BeautifulSoup(source_code, 'lxml')
+ 
+    # Text in given web-page is stored under
+    # the <div> tags with class <entry-content>
+    for each_text in soup.findAll('div', {'class': 'entry-content'}):
+        content = each_text.text
+ 
+        # use split() to break the sentence into
+        # words and convert them into lowercase
+        words = content.lower().split()
+ 
+        for each_word in words:
+            wordlist.append(each_word)
+        clean_wordlist(wordlist)
+ 
+# Function removes any unwanted symbols
+ 
+ 
+def clean_wordlist(wordlist):
+ 
+    clean_list = []
+    for word in wordlist:
+        symbols = "!@#$%^&*()_-+={[}]|\;:\"<>?/., "
+ 
+        for i in range(len(symbols)):
+            word = word.replace(symbols[i], '')
+ 
+        if len(word) > 0:
+            clean_list.append(word)
+    create_dictionary(clean_list)
+ 
+# Creates a dictionary conatining each word's
+# count and top_20 ocuuring words
+ 
+ 
+def create_dictionary(clean_list):
+    word_count = {}
+ 
+    for word in clean_list:
+        if word in word_count:
+            word_count[word] += 1
+        else:
+            word_count[word] = 1
+ 
+    ''' To get the count of each word in
+        the crawled page -->
+ 
+    # operator.itemgetter() takes one
+    # parameter either 1(denotes keys)
+    # or 0 (denotes corresponding values)
+ 
+    for key, value in sorted(word_count.items(),
+                    key = operator.itemgetter(1)):
+        print ("% s : % s " % (key, value))
+ 
+    <-- '''
+ 
+    c = Counter(word_count)
+ 
+    # returns the most occurring elements
+    top = c.most_common(10)
+    print(top)
+ 
+ 
+# Driver code
+if __name__ == '__main__':
+    url = "https://wikipedia.com/"
+    # starts crawling and prints output
+    start(url)
